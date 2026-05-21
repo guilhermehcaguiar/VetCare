82509294c24d11fe1853a9bf150bb4b1adfb92cb
Guilherme Aguiar <guilherme.ghca@hotmail.com>
2026-05-21 00:28:29 -0300

    chore: configura rest framework e permissoes de cors no settings

--- backend/vetcare_api/settings.py
+++ backend/vetcare_api/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Permite que o React (Vite) acesse o Back-end em ambiente de desenvolvimento
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
